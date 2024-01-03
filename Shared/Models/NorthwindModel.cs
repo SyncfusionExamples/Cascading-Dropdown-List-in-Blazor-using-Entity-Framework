7ca4a857cdd1fb6af2277ad75a720351cc49495a
BackiaRaj <backiaraj.kanagaraj@syncfusion.com>
2024-01-03 18:18:03 +0530

    added missing file

---
+++ Shared/Models/NorthwindModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DropDownListWithEF.Shared.Models
+{
+    public class Region
+    {
+        [Key]
+        public int? RegionID { get; set; }
+
+        public string RegionDescription { get; set; }
+    }
+
+    public class Territories
+    {
+        [Key]
+        public string TerritoryID { get; set; }
+
+        public string TerritoryDescription { get; set; }
+        
+        public int? RegionID { get; set; }
+
+    }
+}
